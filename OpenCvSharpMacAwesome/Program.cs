4f15af2aac3d7d31b35a29695b2feaa865a03d76
rim <mot_siva_ordin@hotmail.com>
2018-10-21 22:38:43 +0900

    OpenCvSharpを使ったソースを追加。 Macでは対応してなさそうなので断念。

---
+++ OpenCvSharpMacAwesome/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OpenCvSharp;
+
+namespace OpenCvSharpMacAwesome
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var vm = new Mat("image.png"))
+            {
+                Cv2.ImShow("test",vm);
+                Cv2.WaitKey();
+                Cv2.DestroyAllWindows();
+            }
+        }
+    }
+}
